98649a4a762902f98ad2cc51355fbdf7528522b8
Leandro Coutinho <lescoutinhovr@gmail.com>
2022-12-07 19:57:59 -0300

    using windows and enumerate for day 6

---
+++ src/day6-part2-bad-slide-window.rs
@@ -0,0 +1,30 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let contents = get_file_contents();
+
+	for line in contents.lines() {
+		let line = line.as_bytes();
+		'i:
+		for i in 13..line.len() {
+			for j in i-13..=i {
+				for z in j + 1..=i {
+					if line[j] == line[z] {
+						continue 'i;
+					}
+				}
+			}
+			println!("{}", i + 1);
+			return;
+		}
+	}
+}
+
+fn get_file_contents() -> String {
+	let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    println!("In file {}", file_path);
+    fs::read_to_string(file_path)
+        .expect("Should have been able to read the file")
+}
--- src/day6-part2.rs
+++ src/day6-part2.rs
@@ -1,30 +1,24 @@
+use std::collections::HashSet;
 use std::env;
 use std::fs;
+use std::iter::FromIterator;
 
 fn main() {
     let contents = get_file_contents();
+    let line = contents.lines().next().unwrap().as_bytes();
 
-	for line in contents.lines() {
-		let line = line.as_bytes();
-		'i:
-		for i in 13..line.len() {
-			for j in i-13..=i {
-				for z in j + 1..=i {
-					if line[j] == line[z] {
-						continue 'i;
-					}
-				}
-			}
-			println!("{}", i + 1);
-			return;
-		}
-	}
+    for (i, w) in line.windows(14).enumerate() {
+        let set: HashSet<&u8> = HashSet::from_iter(w);
+        if set.len() == 14 {
+            println!("{}", i + 14);
+            return;
+        }
+    }
 }
 
 fn get_file_contents() -> String {
-	let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
     println!("In file {}", file_path);
-    fs::read_to_string(file_path)
-        .expect("Should have been able to read the file")
+    fs::read_to_string(file_path).expect("Should have been able to read the file")
 }
