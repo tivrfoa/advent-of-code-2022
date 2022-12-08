cbac037c2d92b328d8b7485ea20af306ceb83131
Leandro Coutinho <lescoutinhovr@gmail.com>
2022-12-08 17:29:42 -0300

    refactor: pass iterator to fn

--- src/day8-part2.rs
+++ src/day8-part2.rs
@@ -3,6 +3,28 @@ use std::fs;
 
 const ZERO: u8 = '0' as u8;
 
+fn get_qt_col<I: Iterator<Item = usize>>(grid: &Vec<Vec<u8>>, iter: I, r: usize, c: usize) -> u32 {
+    let mut qt = 0;
+    for j in iter {
+        qt += 1;
+        if grid[r][j] >= grid[r][c] {
+            break;
+        }
+    }
+    qt
+}
+
+fn get_qt_row<I: Iterator<Item = usize>>(grid: &Vec<Vec<u8>>, iter: I, r: usize, c: usize) -> u32 {
+    let mut qt = 0;
+    for j in iter {
+        qt += 1;
+        if grid[j][c] >= grid[r][c] {
+            break;
+        }
+    }
+    qt
+}
+
 fn main() {
     let contents = get_file_contents();
     let mut grid: Vec<Vec<u8>> = vec![];
@@ -24,52 +46,16 @@ fn main() {
     for r in 1..rows - 1 {
         for c in 1..cols - 1 {
             // left
-            let qt_left = {
-                let mut qt = 0;
-                for j in (0..c).rev() {
-                    qt += 1;
-                    if grid[r][j] >= grid[r][c] {
-                        break;
-                    }
-                }
-                qt
-            };
+            let qt_left = get_qt_col(&grid, (0..c).rev(), r, c);
 
             // right
-            let qt_right = {
-                let mut qt = 0;
-                for j in c + 1..cols {
-                    qt += 1;
-                    if grid[r][j] >= grid[r][c] {
-                        break;
-                    }
-                }
-                qt
-            };
+            let qt_right = get_qt_col(&grid, c + 1..cols, r, c);
 
             // bottom
-            let qt_bottom = {
-                let mut qt = 0;
-                for j in r + 1..rows {
-                    qt += 1;
-                    if grid[j][c] >= grid[r][c] {
-                        break;
-                    }
-                }
-                qt
-            };
+            let qt_bottom = get_qt_row(&grid, r + 1..rows, r, c);
 
             // top
-            let qt_top = {
-                let mut qt = 0;
-                for j in (0..r).rev() {
-                    qt += 1;
-                    if grid[j][c] >= grid[r][c] {
-                        break;
-                    }
-                }
-                qt
-            };
+            let qt_top = get_qt_row(&grid, (0..r).rev(), r, c);
 
             let scenic_score = qt_left * qt_right * qt_bottom * qt_top;
             if scenic_score > max {
