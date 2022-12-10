b82de0cc0cd421ede7965a84f7455b4cc7e93772
Leandro Coutinho <lescoutinhovr@gmail.com>
2022-12-10 11:13:28 -0300

    Solved day 9 part 1

--- src/day9.rs
+++ src/day9.rs
@@ -13,79 +13,101 @@ fn visit(visited: &mut [[bool; C]; R], tr: usize, tc: usize) -> u32 {
 }
 
 fn move_dir(dir: char, hr: usize, hc: usize, tr: usize, tc: usize) -> (usize, usize, usize, usize) {
-
-	match dir {
-		'R' => {
-			if hr == tr && hc == tc { // H covers T
-				(hr, hc + 1, tr, tc)
-			} else if hr != tr && hc != tc { // they are in diagonal
-				if hc < tc {
-					(hr, hc + 1, tr, tc)
-				} else { // hc > tc
-					(hr, hc + 1, hr, hc)
-				}
-			} else {
-				if hr != tr { // same column, different row => only H moves
-					(hr, hc + 1, tr, tc)
-				} else {
-					(hr, hc + 1, tr, tc + 1)
-				}
-			}
-		}
-		'L' => {
-			if hr == tr && hc == tc { // H covers T
-				(hr, hc - 1, tr, tc)
-			} else if hr != tr && hc != tc { // they are in diagonal
-				if hc > tc {
-					(hr, hc - 1, tr, tc)
-				} else { // hc < tc
-					(hr, hc - 1, hr, hc)
-				}
-			} else {
-				if hr != tr { // same column, different row => only H moves
-					(hr, hc - 1, tr, tc)
-				} else {
-					(hr, hc - 1, tr, tc - 1)
-				}
-			}
-		}
-		'D' => {
-			if hr == tr && hc == tc { // H covers T
-				(hr + 1, hc, tr, tc)
-			} else if hr != tr && hc != tc { // they are in diagonal
-				if hr < tr {
-					(hr + 1, hc, tr, tc) // they'll be in same row
-				} else { // hr > tr
-					(hr + 1, hc, hr, hc)
-				}
-			} else {
-				if hc != tc { // same row, different column => only H moves
-					(hr + 1, hc, tr, tc)
-				} else {
-					(hr + 1, hc, tr + 1, tc)
-				}
-			}
-		}
-		'U' => {
-			if hr == tr && hc == tc { // H covers T
-				(hr - 1, hc, tr, tc)
-			} else if hr != tr && hc != tc { // they are in diagonal
-				if hr > tr {
-					(hr - 1, hc, tr, tc) // they'll be in same row
-				} else { // hr < tr
-					(hr - 1, hc, hr, hc)
-				}
-			} else {
-				if hc != tc { // same row, different column => only H moves
-					(hr - 1, hc, tr, tc)
-				} else {
-					(hr - 1, hc, tr - 1, tc)
-				}
-			}
-		}
-		d @ _ => panic!("Invalid dir: {d}"),
-	}
-
+    match dir {
+        'R' => {
+            if hr == tr && hc == tc {
+                // H covers T
+                (hr, hc + 1, tr, tc)
+            } else if hr != tr && hc != tc {
+                // they are in diagonal
+                if hc < tc {
+                    (hr, hc + 1, tr, tc)
+                } else {
+                    // hc > tc
+                    (hr, hc + 1, hr, hc)
+                }
+            } else {
+                if hr != tr {
+                    // same column, different row => only H moves
+                    (hr, hc + 1, tr, tc)
+                } else if hc + 1 == tc {
+                    (hr, hc + 1, tr, tc)
+                } else {
+                    (hr, hc + 1, tr, tc + 1)
+                }
+            }
+        }
+        'L' => {
+            if hr == tr && hc == tc {
+                // H covers T
+                (hr, hc - 1, tr, tc)
+            } else if hr != tr && hc != tc {
+                // they are in diagonal
+                if hc > tc {
+                    (hr, hc - 1, tr, tc)
+                } else {
+                    // hc < tc
+                    (hr, hc - 1, hr, hc)
+                }
+            } else {
+                if hr != tr {
+                    // same column, different row => only H moves
+                    (hr, hc - 1, tr, tc)
+                } else if hc - 1 == tc {
+                    (hr, hc - 1, tr, tc)
+                } else {
+                    (hr, hc - 1, tr, tc - 1)
+                }
+            }
+        }
+        'D' => {
+            if hr == tr && hc == tc {
+                // H covers T
+                (hr + 1, hc, tr, tc)
+            } else if hr != tr && hc != tc {
+                // they are in diagonal
+                if hr < tr {
+                    (hr + 1, hc, tr, tc) // they'll be in same row
+                } else {
+                    // hr > tr
+                    (hr + 1, hc, hr, hc)
+                }
+            } else {
+                if hc != tc {
+                    // same row, different column => only H moves
+                    (hr + 1, hc, tr, tc)
+                } else if hr + 1 == tr {
+                    (hr + 1, hc, tr, tc)
+                } else {
+                    (hr + 1, hc, tr + 1, tc)
+                }
+            }
+        }
+        'U' => {
+            if hr == tr && hc == tc {
+                // H covers T
+                (hr - 1, hc, tr, tc)
+            } else if hr != tr && hc != tc {
+                // they are in diagonal
+                if hr > tr {
+                    (hr - 1, hc, tr, tc) // they'll be in same row
+                } else {
+                    // hr < tr
+                    (hr - 1, hc, hr, hc)
+                }
+            } else {
+                if hc != tc {
+                    // same row, different column => only H moves
+                    (hr - 1, hc, tr, tc)
+                } else if hr - 1 == tr {
+                    (hr - 1, hc, tr, tc)
+                } else {
+                    (hr - 1, hc, tr - 1, tc)
+                }
+            }
+        }
+        d @ _ => panic!("Invalid dir: {d}"),
+    }
 }
 
 pub fn solve(input: String) -> u32 {
@@ -103,14 +125,14 @@ pub fn solve(input: String) -> u32 {
         let dir = tokens[0].chars().next().unwrap();
         let steps: usize = tokens[1].parse().unwrap();
 
-		for _ in 0..steps {
-			let (new_hr, new_hc, new_tr, new_tc) = move_dir(dir, hr, hc, tr, tc);
-			hr = new_hr;
-			hc = new_hc;
-			tr = new_tr;
-			tc = new_tc;
-			ans += visit(&mut visited, tr, tc);
-		}
+        for _ in 0..steps {
+            let (new_hr, new_hc, new_tr, new_tc) = move_dir(dir, hr, hc, tr, tc);
+            hr = new_hr;
+            hc = new_hc;
+            tr = new_tr;
+            tc = new_tc;
+            ans += visit(&mut visited, tr, tc);
+        }
     }
 
     ans
@@ -130,7 +152,7 @@ mod tests {
     fn part1_input() {
         let input = util::read_file("inputs/input-day9.txt");
 
-		// failed answers: 6846, 6821
+        // failed answers: 6846, 6821
         assert_eq!(574080, solve(input)); // got 6846 which is wrong
     }
 
