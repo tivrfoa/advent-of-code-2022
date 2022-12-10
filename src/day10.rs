4176b493378568d75858d3fa89043eca9bc05f51
Leandro Coutinho <lescoutinhovr@gmail.com>
2022-12-10 20:01:39 -0300

    "solved" day 10 part 2 - letters are not clear. I need to fix it

--- src/day10.rs
+++ src/day10.rs
@@ -24,6 +24,52 @@ pub fn solve(input: String) -> i32 {
     ans
 }
 
+pub fn solve_part2(input: String) -> Vec<String> {
+    let mut cycles = 0;
+    let mut x: i32 = 1;
+    let mut crt: Vec<String> = vec![];
+    let mut crt_line = String::new();
+    for line in input.lines() {
+        let tokens: Vec<&str> = line.split_ascii_whitespace().collect();
+        let op = tokens[0];
+
+        if cycles == x || cycles + 1 == x || cycles - 1 == x {
+            crt_line.push('#');
+        } else {
+            crt_line.push('.');
+        }
+
+        if op == "noop" {
+            cycles += 1;
+        } else {
+            cycles += 1;
+            if cycles == x || cycles + 1 == x || cycles - 1 == x {
+                crt_line.push('#');
+            } else {
+                crt_line.push('.');
+            }
+            if cycles == 40 {
+                cycles = 0;
+                crt.push(crt_line);
+                crt_line = String::new();
+            }
+            cycles += 1;
+            x += tokens[1].parse::<i32>().unwrap();
+        }
+        if cycles == 40 {
+            cycles = 0;
+            crt.push(crt_line);
+            crt_line = String::new();
+        }
+    }
+
+    for i in 0..crt.len() {
+        println!("{}", crt[i]);
+    }
+
+    crt
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -40,9 +86,31 @@ mod tests {
         assert_eq!(14060, solve(input));
     }
 
-    // #[test]
-    // fn part2_input() {
-    //     let input = util::read_file("inputs/input-day9.txt");
-    //     assert_eq!(2717, solve(input, 10));
-    // }
+    #[test]
+    fn part2_sample() {
+        let expected: Vec<String> = vec![
+            "##..##..##..##..##..##..##..##..##..##..".into(),
+            "###...###...###...###...###...###...###.".into(),
+            "####....####....####....####....####....".into(),
+            "#####.....#####.....#####.....#####.....".into(),
+            "######......######......######......####".into(),
+            "#######.......#######.......#######.....".into(),
+        ];
+        let input = util::read_file("inputs/sample-day10.txt");
+        assert_eq!(expected, solve_part2(input));
+    }
+
+    #[test]
+    fn part2_input() {
+        let expected: Vec<String> = vec![
+            "##..##..##..##..##..##..##..##..##..##..".into(),
+            "###...###...###...###...###...###...###.".into(),
+            "####....####....####....####....####....".into(),
+            "#####.....#####.....#####.....#####.....".into(),
+            "######......######......######......####".into(),
+            "#######.......#######.......#######.....".into(),
+        ];
+        let input = util::read_file("inputs/input-day10.txt");
+        assert_eq!(expected, solve_part2(input));
+    }
 }
