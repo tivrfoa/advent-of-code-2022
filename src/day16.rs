3b2e38f1d36735fda2e809b111d642794417f7d8
Leandro Coutinho <lescoutinhovr@gmail.com>
2022-12-18 17:16:25 -0300

    day 16: solved sample

--- src/day16.rs
+++ src/day16.rs
@@ -1,104 +1,155 @@
 use crate::util;
 
 use std::collections::HashMap;
-use std::collections::VecDeque;
 
 #[derive(Debug)]
 struct Valve {
-	idx: usize,
-	flow_rate: usize,
-	conn_indexes: VecDeque<usize>,
+    label: String, // just for debugging
+    idx: usize,
+    flow_rate: usize,
+    conn_indexes: Vec<usize>,
 }
 
 fn parse_input(input: String) -> Vec<Valve> {
-	let mut valves: Vec<Valve> = vec![];
+    let mut valves: Vec<Valve> = vec![];
     let mut valves_index_map: HashMap<&str, usize> = HashMap::new();
-	let mut next_idx = 0;
+    let mut next_idx = 0;
     for line in input.lines() {
         let (_, rem) = line.split_once("Valve ").unwrap();
-		let (valve_label, rem) = rem.split_once(" has flow rate=").unwrap();
-		let (flow_rate, rem) = rem.split_once(';').unwrap();
-		let conn = match rem.split_once("to valves ") {
-			Some((_, c)) => c,
-			None => rem.split_once("to valve ").unwrap().1,
-		};
+        let (valve_label, rem) = rem.split_once(" has flow rate=").unwrap();
+        let (flow_rate, rem) = rem.split_once(';').unwrap();
+        let conn = match rem.split_once("to valves ") {
+            Some((_, c)) => c,
+            None => rem.split_once("to valve ").unwrap().1,
+        };
 
-		if !valves_index_map.contains_key(valve_label) {
-			valves_index_map.insert(valve_label, next_idx);
-			next_idx += 1;
-		}
+        if !valves_index_map.contains_key(valve_label) {
+            valves_index_map.insert(valve_label, next_idx);
+            next_idx += 1;
+        }
 
-		let mut conn_indexes: VecDeque<usize> = VecDeque::new();
-		for c in conn.split(", ") {
-			match valves_index_map.get(c) {
-				Some(idx) => {
-					conn_indexes.push_back(*idx);
-				}
-				None => {
-					conn_indexes.push_back(next_idx);
-					valves_index_map.insert(c, next_idx);
-					next_idx += 1;
-				}
-			}
-		}
+        let mut conn_indexes: Vec<usize> = Vec::new();
+        for c in conn.split(", ") {
+            match valves_index_map.get(c) {
+                Some(idx) => {
+                    conn_indexes.push(*idx);
+                }
+                None => {
+                    conn_indexes.push(next_idx);
+                    valves_index_map.insert(c, next_idx);
+                    next_idx += 1;
+                }
+            }
+        }
 
-		valves.push(Valve {
-			idx: *valves_index_map.get(valve_label).unwrap(),
-			flow_rate: flow_rate.parse().unwrap(),
-			conn_indexes,
-		});
+        valves.push(Valve {
+            label: valve_label.into(),
+            idx: *valves_index_map.get(valve_label).unwrap(),
+            flow_rate: flow_rate.parse().unwrap(),
+            conn_indexes,
+        });
     }
 
-	valves
+    // set in correct index
+    valves.sort_by(|a, b| a.idx.cmp(&b.idx));
+
+    valves
 }
 
-fn bt(valves: &mut Vec<Valve>, used_valves: &mut Vec<bool>, rem_minutes: usize, curr_idx: usize) -> usize {
-	if rem_minutes == 1 {
-		return 0;
-	}
-	let mut max = 0;
+#[derive(PartialEq)]
+enum Action {
+    Open,
+    Move,
+    Start,
+    Stay,
+}
 
-	// option 1 - open valve
-	if !used_valves[curr_idx] && valves[curr_idx].flow_rate > 0 {
-		used_valves[curr_idx] = true;
-		let pressure = bt(valves, used_valves, rem_minutes - 1, curr_idx) +
-			valves[curr_idx].flow_rate * (rem_minutes - 1);
-		if pressure > max {
-			max = pressure;
-		}
+fn bt(
+    valves: &mut Vec<Valve>,
+    used_valves: &mut Vec<bool>,
+    minutes: usize,
+    previous_idx: usize,
+    curr_idx: usize,
+    last_action: Action,
+    curr_flow: usize,
+) -> usize {
+    if minutes == 30 {
+        return curr_flow;
+    }
+    let mut max = 0;
 
-		// undo
-		used_valves[curr_idx] = false;
-	}
+    // option 1 - open valve
+    if !used_valves[curr_idx] && valves[curr_idx].flow_rate > 0 {
+        used_valves[curr_idx] = true;
 
-	// option 2 - move to some connection
-	let len = valves[curr_idx].conn_indexes.len();
-	for _ in 0..len {
-		let idx = valves[curr_idx].conn_indexes.pop_front().unwrap();
-		let pressure = bt(valves, used_valves, rem_minutes - 1, idx);
-		if pressure > max {
-			max = pressure;
-		}
+        let pressure = bt(
+            valves,
+            used_valves,
+            minutes + 1,
+            previous_idx,
+            curr_idx,
+            Action::Open,
+            curr_flow + valves[curr_idx].flow_rate,
+        );
+        if pressure > max {
+            max = pressure;
+        }
 
-		// undo
-		valves[curr_idx].conn_indexes.push_back(idx);
-	}
+        // undo
+        used_valves[curr_idx] = false;
+    }
 
-	max
+    // option 2 - move to some connection
+    let len = valves[curr_idx].conn_indexes.len();
+    if len == 0 {
+        let pressure = bt(
+            valves,
+            used_valves,
+            minutes + 1,
+            curr_idx,
+            curr_idx,
+            Action::Stay,
+            curr_flow,
+        );
+        if pressure > max {
+            max = pressure;
+        }
+    } else {
+        for i in 0..len {
+            let idx = valves[curr_idx].conn_indexes[i];
+            if idx == previous_idx && last_action == Action::Move {
+                continue;
+            }
+            let pressure = bt(
+                valves,
+                used_valves,
+                minutes + 1,
+                curr_idx,
+                idx,
+                Action::Move,
+                curr_flow,
+            );
+            if pressure > max {
+                max = pressure;
+            }
+        }
+    }
+
+    curr_flow + max
 }
 
 pub fn solve(input: String) -> usize {
-	let mut valves = parse_input(input);
-	let mut used_valves = vec![false; valves.len()];
+    let mut valves = parse_input(input);
+    let mut used_valves = vec![false; valves.len()];
 
-	dbg!(&valves);
+    dbg!(&valves);
 
-	// I'll use backtrack
-	bt(&mut valves, &mut used_valves, 30, 0)
+    // I'll use backtrack
+    bt(&mut valves, &mut used_valves, 1, 0, 0, Action::Start, 0)
 }
 
 pub fn solve_part2(input: String, max: i64) -> i64 {
-	0
+    0
 }
 
 #[allow(dead_code)]
@@ -118,11 +169,11 @@ mod tests {
         assert_eq!(1651, solve(input));
     }
 
-    //#[test]
-    //fn part1_input() {
-    //    let input = util::read_file("inputs/day16.txt");
-    //    assert_eq!(5181556, solve(input));
-    //}
+    #[test]
+    fn part1_input() {
+        let input = util::read_file("inputs/day16.txt");
+        assert_eq!(5181556, solve(input));
+    }
 
     //#[test]
     //fn part2_sample() {
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-mod day15_based_on_elizarov;
-mod util;
+// mod day15_based_on_elizarov;
+// mod util;
 
 fn main() {
-    let input = util::read_file("inputs/day15.txt");
-    println!("{}", day15_based_on_elizarov::solve_part2(input, 4000000));
+    //    let input = util::read_file("inputs/day15.txt");
+    //    println!("{}", day15_based_on_elizarov::solve_part2(input, 4000000));
 }
-
-// fn main() {}
