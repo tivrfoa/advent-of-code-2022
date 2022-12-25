98016376662ba01889b250ae5427d2a504f197a3
Leandro Coutinho <lescoutinhovr@gmail.com>
2022-12-25 18:02:13 -0300

    error[E0502]: cannot borrow `f` as mutable because it is also borrowed as immutable    --> src/day17_elizarov.rs:154:13     | 58  |     let test = |si: usize, i0: usize, j0: usize, len: usize| -> bool {     |                ----------------------------------------------------- immutable borrow occurs here ... 67  |                 if i < len && f[i][j] {     |                               - first borrow occurs due to use of `f` in closure ... 131 |         assert!(test(si, i, j, f.len()));     |                 ---- immutable borrow later used here ... 154 |         put(&mut f, si, i, j);     |             ^^^^^^ mutable borrow occurs here

--- src/day17_elizarov.rs
+++ src/day17_elizarov.rs
@@ -2,70 +2,69 @@ use crate::util;
 
 use std::collections::HashMap;
 
-/*fn shape<T>(a: usize, u: T) -> bool
-where T: Fn(usize, usize) -> bool {
+fn shape(a: usize, mut u: impl FnMut(usize, usize)) {
     match a {
-        0 => u(0, 0) && u(0, 1) && u(0, 2) && u(0, 3),
-        1 => u(0, 1) && u(1, 0) && u(1, 1) && u(1, 2) && u(2, 1),
-        2 => u(0, 0) && u(0, 1) && u(0, 2) && u(1, 2) && u(2, 2),
-        3 => u(0, 0) && u(1, 0) && u(2, 0) && u(3, 0),
-        4 => u(0, 0) && u(0, 1) && u(1, 0) && u(1, 1),
+        0 => {
+            u(0, 0);
+            u(0, 1);
+            u(0, 2);
+            u(0, 3);
+        }
+        1 => {
+            u(0, 1);
+            u(1, 0);
+            u(1, 1);
+            u(1, 2);
+            u(2, 1);
+        }
+        2 => {
+            u(0, 0);
+            u(0, 1);
+            u(0, 2);
+            u(1, 2);
+            u(2, 2);
+        }
+        3 => {
+            u(0, 0);
+            u(1, 0);
+            u(2, 0);
+            u(3, 0);
+        }
+        4 => {
+            u(0, 0);
+            u(0, 1);
+            u(1, 0);
+            u(1, 1);
+        }
         _ => panic!("shape {a}"),
     }
-}*/
+}
+
+fn put(f: &mut Vec<Vec<bool>>, si: usize, i0: usize, j0: usize) {
+    shape(si, |di, dj| {
+        let i = i0 + di;
+        let j = j0 + dj;
+        while i > f.len() {
+            f.push(vec![false; 7]);
+        }
+        f[i][j] = true;
+    });
+}
 
 pub fn solve(input: String, rocks: usize) -> usize {
-    let input = input.trim();
+    let jets: Vec<char> = input.trim().chars().collect();
     let mut f: Vec<Vec<bool>> = vec![];
 
-    let shape = |a, u: &dyn Fn(usize, usize)| {
-        match a {
-            0 => {
-                u(0, 0);
-                u(0, 1);
-                u(0, 2);
-                u(0, 3);
-            }
-            1 => {
-                u(0, 1);
-                u(1, 0);
-                u(1, 1);
-                u(1, 2);
-                u(2, 1);
-            }
-            2 => {
-                u(0, 0);
-                u(0, 1);
-                u(0, 2);
-                u(1, 2);
-                u(2, 2);
-            }
-            3 => {
-                u(0, 0);
-                u(1, 0);
-                u(2, 0);
-                u(3, 0);
-            }
-            4 => {
-                u(0, 0);
-                u(0, 1);
-                u(1, 0);
-                u(1, 1);
-            }
-            _ => panic!("shape {a}"),
-        }
-    };
-
-    let test = |si: usize, i0: usize, j0: usize| -> bool {
+    let test = |si: usize, i0: usize, j0: usize, len: usize| -> bool {
         if i0 < 0 { return false; }
         let mut ok = true;
-        shape(si, &|di, dj| {
+        shape(si, |di, dj| {
             let i = i0 + di;
             let j = j0 + dj;
             if j > 6 {
                 ok = false;
             } else {
-                if i < f.len() && f[i][j] {
+                if i < len && f[i][j] {
                     ok = false;
                 }
             }
@@ -73,18 +72,109 @@ pub fn solve(input: String, rocks: usize) -> usize {
         ok
     };
 
-    let put = |si, i0, j0| {
-        shape(si, &|di, dj| {
-            let i = i0 + di;
-            let j = j0 + dj;
-            while i > f.len() {
-                f.push(vec![false; 7]);
+    let mut pj = 0;
+    let mut sn = 5;
+    let mut si = sn - 1;
+
+    let mask = || -> usize {
+        let mut m = 0;
+        let mut k = 1;
+        for i in 0..7 {
+            for j in 0..6 {
+                let last_index = f.len() - 1;
+                if i > last_index || f[last_index - i][j] {
+                    m = m | k;
+                }
+                k <<= 1;
             }
-            f[i][j] = true;
-        });
+        }
+        m
     };
 
-    0
+    let mut us: Vec<Upd> = vec![];
+    let mut ui: HashMap<Upd, usize> = HashMap::new();
+    let mut ans = 0;
+
+    let mut rec = |u: Upd| -> bool {
+        let rn = us.len();
+        let prev = *match ui.get(&u) {
+            Some(idx) => idx,
+            None => {
+                ui.insert(u, rn);
+                us.push(u.clone());
+                return false;
+            }
+        };
+        let clen = rn - prev;
+        let rem = rocks - rn;
+        let mut ds0 = 0;
+        let i_rest = prev + (rem % clen);
+        for k in 0..rn {
+            ds0 += us[k].ds;
+        }
+        for k in prev..i_rest {
+            ds0 += us[k].ds;
+        };
+        let mut ds_cycle = 0;
+        for k in prev..rn {
+            ds_cycle += us[k].ds;
+        }
+        ans = ds0 + (rem / clen) * ds_cycle;
+        true
+    };
+
+    for rn in 0..rocks {
+        si = (si + 1) % sn;
+        let s0 = f.len();
+        let mut i = s0 + 3;
+        let mut j = 2;
+        assert!(test(si, i, j, f.len()));
+        loop {
+            let d = jets[pj];
+            pj = (pj + 1) % jets.len();
+            match d {
+                '<' => {
+                    if j > 0 && test(si, i, j - 1, f.len()) {
+                        j -= 1;
+                    }
+                },
+                '>' => {
+                    if test(si, i, j + 1, f.len()) {
+                        j += 1;
+                    }
+                },
+                _ => panic!("{d}"),
+            };
+            if !test(si, i - 1, j, f.len()) {
+                break;
+            }
+            i -= 1;
+        }
+        let m1 = mask();
+        put(&mut f, si, i, j);
+        if rec(Upd {
+            m1,
+            si,
+            pj,
+            ds: f.len() - s0,
+            i: s0 - i,
+            j,
+        }) {
+            break;
+        }
+    }
+
+    ans
+}
+
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+struct Upd {
+    m1: usize,
+    si: usize,
+    pj: usize,
+    ds: usize,
+    i: usize,
+    j: usize,
 }
 
 
