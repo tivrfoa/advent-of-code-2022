48570ab09f73303e55eefec21ea12c0d483d0b7b
Leandro Coutinho <lescoutinhovr@gmail.com>
2022-12-31 11:02:45 -0300

    day 21: solving sample

--- src/day21.rs
+++ src/day21.rs
@@ -14,16 +14,16 @@ All jobs (monkeys) are unique!!!
 */
 
 fn solve(jobs: &mut HashMap<String, Job>, key: String) -> i32 {
-    let job = jobs.get(&key).unwrap();
+    let job = jobs.get(&key).unwrap().clone();
 
     if let Some(r) = job.result {
         r
     } else {
-        let (a, op, b) = job.operation.unwrap();
-        let a = solve(jobs, a);
-        let b = solve(jobs, b);
-        let result = op.calc(a, b);
-        job.result = Some(result);
+        let calc = job.operation.unwrap();
+        let a = solve(jobs, calc.left);
+        let b = solve(jobs, calc.right);
+        let result = calc.op.calc(a, b);
+        jobs.get_mut(&key).unwrap().result = Some(result);
         result
     }
 }
@@ -39,13 +39,20 @@ pub fn part2(input: String) -> String {
     todo!()
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct Job {
     result: Option<i32>,
-    operation: Option<(String, Op, String)>,
+    operation: Option<Calc>,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
+struct Calc {
+    left: String,
+    op: Op,
+    right: String,
+}
+
+#[derive(Clone, Debug)]
 enum Op {
     Add,
     Sub,
@@ -95,11 +102,11 @@ fn parse(input: String) -> HashMap<String, Job> {
                 job_name.into(),
                 Job {
                     result: None,
-                    operation: Some((
-                        tokens[1].to_string(),
-                        Op::from_str(tokens[2]),
-                        tokens[3].to_string(),
-                    )),
+                    operation: Some(Calc {
+                        left: tokens[1].to_string(),
+                        op: Op::from_str(tokens[2]),
+                        right: tokens[3].to_string(),
+                    }),
                 },
             );
         }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
 #![feature(int_roundings)]
 #[allow(dead_code, unused_imports)]
-mod day20;
+mod day21;
 mod util;
 
 fn main() {
     // test_sample();
-    test_input();
+    // test_input();
     //test_part2();
 }
 
 fn test_sample() {
-    let input = util::read_file("inputs/day20-sample.txt");
-    let ans = day20::part1(input);
+    let input = util::read_file("inputs/day21-sample.txt");
+    let ans = day21::part1(input);
     assert_eq!("3", ans);
     println!("{}", ans);
 }
 
 fn test_input() {
-    let input = util::read_file("inputs/day20.txt");
-    // let input = util::read_file("inputs/day20-simple1.txt");
-    println!("{}", day20::part1(input));
+    let input = util::read_file("inputs/day21.txt");
+    // let input = util::read_file("inputs/day21-simple1.txt");
+    println!("{}", day21::part1(input));
 }
 
 fn test_part2() {
-    let input = util::read_file("inputs/day20.txt");
-    println!("{}", day20::part2(input));
+    let input = util::read_file("inputs/day21.txt");
+    println!("{}", day21::part2(input));
 }
