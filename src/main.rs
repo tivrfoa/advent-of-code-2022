3cc92c85ef9a5bb2f14de9da9cf5d5e1cbf6c679
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-01-02 08:08:55 -0300

    fix running from stdin so it works with pipe |

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,35 @@
 #![feature(int_roundings)]
 #[allow(dead_code, unused_imports)]
-mod day22p2;
+mod day22;
 mod util;
 
 use std::io::{self, BufRead};
 
 fn main() {
-    test_sample();
+    // test_sample();
+    run_stdin();
 }
 
 fn run_stdin() {
     let mut input = String::new();
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        input.push_str(&line.unwrap());
+    loop {
+        let mut line = String::new();
+        match io::stdin().read_line(&mut line) {
+            Ok(len) => if len == 0 {
+                break;
+            } else {
+                input.push_str(&line);
+            }
+            Err(error) => {
+                eprintln!("error: {}", error);
+                break;
+            }
+        }
     }
-    println!("{}", day22p2::part2(input));
+    println!("{}", day22::part1(input));
 }
 
 fn test_sample() {
     let input = util::read_file("inputs/day22.txt");
-    println!("{}", day22p2::part2(input));
+    println!("{}", day22::part1(input));
 }
