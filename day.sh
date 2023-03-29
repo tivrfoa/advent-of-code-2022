634e096b08b86afa31e047e0d604c41a59dd3c0a
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-03-29 20:22:07 -0300

    change script template to handle year

--- day.sh
+++ day.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cp src/template.rs src/day$1.rs
+# $1 day
+# $2 year
+cp src/template.rs src/y$2/day$1.rs
 
-sed -i "s/ayX/ay${1}/g" src/day$1.rs
+sed -i "s/ayX/ay${1}/g" src/y$2/day$1.rs
 
-vim src/day$1.rs
+vim src/y$2/day$1.rs
 
---
+++ src/y2021/day1.rs
@@ -0,0 +1,76 @@
+use crate::util;
+
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::cmp::Ordering;
+use std::fmt::{Debug, Display};
+use std::iter::zip;
+
+use crate::aoc::AOC;
+
+fn part1(input: String) -> String {
+    "".into()
+}
+
+fn part2(input: String) -> String {
+    todo!()
+}
+
+fn parse(input: String) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_sample() {
+        let input = util::read_file("inputs/day1-sample.txt");
+        assert_eq!("", part1(input));
+    }
+
+    //#[test]
+    //fn part1_input() {
+    //    let input = util::read_file("inputs/day1.txt");
+    //    assert_eq!("", part1(input));
+    //}
+
+    //#[test]
+    //fn part2_sample() {
+    //    let input = util::read_file("inputs/day1-sample.txt");
+    //    assert_eq!("", part2(input));
+    //}
+
+    //#[test]
+    //fn part2_input() {
+    //    let input = util::read_file("inputs/day1.txt");
+    //    assert_eq!("", part2(input));
+    //}
+}
+
+#[allow(dead_code)]
+fn dbg<T: Debug + Display>(grid: &[Vec<T>]) {
+    for item in grid {
+        println!("{item:?}");
+    }
+}
+
+pub struct Day1 {}
+
+impl AOC for Day1 {
+    fn part1(&self, input: Option<String>, args: Vec<String>) -> String {
+        let input = match input {
+            Some(input) => input,
+            None => util::read_file("inputs/day1.txt"),
+        };
+        part1(input)
+    }
+
+    fn part2(&self, input: Option<String>, args: Vec<String>) -> String {
+        let input = match input {
+            Some(input) => input,
+            None => util::read_file("inputs/day1.txt"),
+        };
+        part2(input)
+    }
+}
