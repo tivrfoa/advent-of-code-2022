9bfbe70ad57a01a401aebc2816f75eb8018bf928
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-04-04 16:36:58 -0300

    nice match binding @ but it does not work for this case =)

--- src/y2021/day8.rs
+++ src/y2021/day8.rs
@@ -17,7 +17,6 @@ fn part1(input: String) -> String {
     let mut qt_uniq = 0;
 
     for right_words in &right {
-        // let map: Vec<Vec<char>> = vec![vec![]; 10];
         for word in right_words {
             match word.len() {
                 2 | 3 | 4 | 7 => qt_uniq += 1,
@@ -26,13 +25,52 @@ fn part1(input: String) -> String {
         }
     }
 
-    // dbg!(&left, &right);
-
     qt_uniq.to_string()
 }
 
 fn part2(input: String) -> String {
-    "".into()
+    let nums_map: [Vec<char>; 10] = [
+        vec!['a', 'b', 'c', 'e', 'f', 'g'],
+        vec!['c', 'f'],
+        vec!['a', 'c', 'd', 'e', 'g'],
+        vec!['a', 'c', 'd', 'f', 'g'],
+        vec!['b', 'c', 'd', 'f'],
+        vec!['a', 'b', 'd', 'f', 'g'],
+        vec!['a', 'b', 'd', 'e', 'f', 'g'],
+        vec!['a', 'c', 'f'],
+        vec!['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+        vec!['a', 'b', 'c', 'd', 'f', 'g'],
+    ];
+    let mut left: Vec<Vec<&str>> = vec![];
+    let mut right: Vec<Vec<&str>> = vec![];
+    for line in input.lines() {
+        let tmp = line.split_once(" | ").unwrap();
+        left.push(tmp.0.split_ascii_whitespace().collect());
+        right.push(tmp.1.split_ascii_whitespace().collect());
+    }
+
+    let mut sum = 0;
+
+    for left_words in &left {
+        let mut map: Vec<Vec<char>> = vec![vec![]; 10];
+        for word in left_words {
+            match word.len() {
+                len @ (2 | 3 | 4 | 7) if map[len].is_empty()  => {
+                    map[len] = word.chars().collect();
+                }
+                _ => (),
+            }
+        }
+
+        // comparing letters in map[2] with map[7] we will know
+        // which is letter a
+        let a: char = *map[7].iter().filter(|c| !map[2].contains(c)).next().unwrap();
+        dbg!(a);
+        dbg!(&map[2], &map[7]);
+    }
+
+
+    sum.to_string()
 }
 
 #[cfg(test)]
@@ -48,20 +86,20 @@ mod tests {
     #[test]
     fn part1_input() {
         let input = util::read_file("inputs/2021/day8.txt");
-        assert_eq!("", part1(input));
+        assert_eq!("375", part1(input));
     }
 
     #[test]
     fn part2_sample() {
         let input = util::read_file("inputs/2021/day8-sample.txt");
-        assert_eq!("", part2(input));
+        assert_eq!("61229", part2(input));
     }
 
-    #[test]
-    fn part2_input() {
-        let input = util::read_file("inputs/2021/day8.txt");
-        assert_eq!("", part2(input));
-    }
+    //#[test]
+    //fn part2_input() {
+    //    let input = util::read_file("inputs/2021/day8.txt");
+    //    assert_eq!("", part2(input));
+    //}
 }
 
 #[allow(dead_code)]
@@ -105,22 +143,3 @@ where
 {
     *vec.iter().min().unwrap()
 }
-// pub struct Day8 {}
-// 
-// impl AOC for Day8 {
-//     fn part1(&self, input: Option<String>, args: Vec<String>) -> String {
-//         let input = match input {
-//             Some(input) => input,
-//             None => util::read_file("inputs/2021/day8.txt"),
-//         };
-//         part1(input)
-//     }
-// 
-//     fn part2(&self, input: Option<String>, args: Vec<String>) -> String {
-//         let input = match input {
-//             Some(input) => input,
-//             None => util::read_file("inputs/2021/day8.txt"),
-//         };
-//         part2(input)
-//     }
-// }
