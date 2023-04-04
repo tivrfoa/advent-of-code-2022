2d2f4715e0feafa700a68b6f5742eb8c9f8bb016
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-04-04 20:54:50 -0300

    2021-day8-p2: solved

--- src/y2021/day8.rs
+++ src/y2021/day8.rs
@@ -37,7 +37,7 @@ fn part2(input: String) -> String {
         right.push(tmp.1.split_ascii_whitespace().collect());
     }
 
-    let mut sum: u32 = 0;
+    let mut sum = 0;
 
     for (left_words, right_words) in left.iter().zip(right.iter()) {
         let mut map: Vec<Vec<char>> = vec![vec![]; 10];
@@ -51,126 +51,73 @@ fn part2(input: String) -> String {
             }
         }
 
-        // comparing letters 1 and 7 we will know
-        // which is letter a
-        let a: char = *map[7]
-            .iter()
-            .filter(|c| !map[1].contains(c))
-            .next()
-            .unwrap();
+        let cf = map[1].clone();
+        let bd = map[4].minus(&map[1]);
 
-        // we can get letters b and d comparing 1 and 4,
-        // although we don't know the order yet
-        let bd: Vec<char> = map[4]
-            .iter()
-            .filter(|c| !map[1].contains(c))
-            .map(|c| *c)
-            .collect();
-
-        // e g = 8 - 4 - a
-        let eg: Vec<char> = map[8]
-            .iter()
-            .filter(|c| **c != a && !map[4].contains(c))
-            .map(|c| *c)
-            .collect();
-
-        map[2] = left_words
-            .iter()
-            .filter(|w| w.len() == 5 && w.contains(eg[0]) && w.contains(eg[1]))
-            .next()
-            .unwrap()
-            .chars()
-            .collect();
-
-        let b: char = *bd.iter()
-            .filter(|c| !map[2].contains(c))
-            .next()
-            .unwrap();
-        let d: char = *bd.iter().filter(|&c| *c != b).next().unwrap();
-
-        map[5] = left_words
-            .iter()
-            .filter(|w| w.len() == 5 && w.contains(b) && w.contains(d))
-            .next()
-            .unwrap()
-            .chars()
-            .collect();
-
-        // c = 4 - 5
-        let c: char = *map[4]
-            .iter()
-            .filter(|c| !map[5].contains(c))
-            .next()
-            .unwrap();
-
-        let f: char = *map[1].iter().filter(|&c0| *c0 != c).next().unwrap();
-
-        // find 0, 6 and 9
         for word in left_words {
-            if word.len() == 6 {
-                if word.contains(eg[0]) && word.contains(eg[1]) {
-                    if word.contains(c) {
+            match word.len() {
+                6 => {
+                    if word.intersect(&bd).len() == 1 {
                         map[0] = word.chars().collect();
-                    } else {
+                    } else if word.intersect(&cf).len() == 1 {
                         map[6] = word.chars().collect();
+                    } else {
+                        map[9] = word.chars().collect();
                     }
-                } else {
-                    map[9] = word.chars().collect();
-                }
-            }
-        }
-
-        // e = 6 - 9
-        let e: char = *map[6]
-            .iter()
-            .filter(|c| !map[9].contains(c))
-            .next()
-            .unwrap();
-
-        let g: char = eg.into_iter().filter(|&c| c != e).next().unwrap();
-
-
-        let mut word = String::new();
-        for right_word in right_words {
-            match right_word.len() {
-                2 => {
-                    word.push('1');
-                }
-                3 => {
-                    word.push('7');
-                }
-                4 => {
-                    word.push('4');
                 }
                 5 => {
-                    if word.contains(b) {
-                        word.push('5');
-                    } else if word.contains(f) {
-                        word.push('3');
+                    if word.intersect(&cf).len() == 2 {
+                        map[3] = word.chars().collect();
+                    } else if word.intersect(&bd).len() == 2 {
+                        map[5] = word.chars().collect();
                     } else {
-                        word.push('2');
+                        map[2] = word.chars().collect();
                     }
                 }
-                6 => {
-                    if !word.contains(d) {
-                        word.push('0');
-                    } else if word.contains(e) {
-                        word.push('6');
-                    } else {
-                        word.push('9');
-                    }
-                }
-                7 => {
-                    word.push('8');
-                }
-                l @ _ => panic!("invalid length {l}"),
+                _ => (),
             }
         }
-        dbg!(&word);
-        sum += word.parse::<u32>().unwrap();
+
+        for m in &mut map { m.sort(); }
+
+        let mut n = 0;
+
+        for rw in right_words {
+            let mut rw: Vec<char> = rw.chars().collect();
+            rw.sort();
+            for (i, m) in map.iter().enumerate() {
+                if *m == rw {
+                    n = n * 10 + i;
+                }
+            }
+        }
+
+        dbg!(&n);
+        sum += n;
     }
 
     sum.to_string()
+}
+
+trait Minus {
+    fn minus(&self, b: &[char]) -> Vec<char>;
+}
+impl Minus for Vec<char> {
+    fn minus(&self, b: &[char]) -> Vec<char> {
+        self.iter()
+            .filter(|c| !b.contains(c))
+            .map(|c| *c)
+            .collect()
+    }
+}
+
+trait Intersect {
+    fn intersect(&self, b: &[char]) -> Vec<char>;
+}
+impl Intersect for &str {
+    fn intersect(&self, b: &[char]) -> Vec<char> {
+        self.chars().filter(|c| b.contains(c)).collect()
+    }
 }
 
 #[cfg(test)]
@@ -195,11 +142,11 @@ mod tests {
         assert_eq!("61229", part2(input));
     }
 
-    //#[test]
-    //fn part2_input() {
-    //    let input = util::read_file("inputs/2021/day8.txt");
-    //    assert_eq!("", part2(input));
-    //}
+    #[test]
+    fn part2_input() {
+       let input = util::read_file("inputs/2021/day8.txt");
+       assert_eq!("1019355", part2(input));
+    }
 }
 
 #[allow(dead_code)]
