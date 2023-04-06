47239f7084e27ab2db9452448b61e97addbcdfb1
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-04-06 19:49:43 -0300

    day script: create files and update mod.rs

--- day.sh
+++ day.sh
@@ -2,6 +2,11 @@
 
 # $1 day
 # $2 year
+
+echo "mod day${1};" > "src/y${2}/mod.rs"
+
+touch inputs/$2/day$1.txt inputs/$2/day$1-sample.txt
+
 cp src/template.rs src/y$2/day$1.rs
 
 sed -i "s/dayX/${2}\/day${1}/g" src/y$2/day$1.rs
