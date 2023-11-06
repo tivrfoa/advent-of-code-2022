cc438de57ad2d100e4dcc1e4316ef81dbe9b3ab3
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-11-05 23:55:03 -0300

    solved p2

--- src/y2020/day8.rs
+++ src/y2020/day8.rs
@@ -30,13 +30,13 @@ fn part1(input: String) -> String {
 		match ops[idx].0 {
 			"acc" => {
 				v += ops[idx].1;
-				idx = (idx + 1) % len;
+				idx += 1;
 			}
 			"jmp" => {
 				idx = (idx as i32 + ops[idx].1) as usize;
 			}
 			"nop" => {
-				idx = (idx + 1) % len;
+				idx += 1;
 			}
 			_ => panic!("{}", ops[idx].0),
 		}
@@ -46,7 +46,53 @@ fn part1(input: String) -> String {
 }
 
 fn part2(input: String) -> String {
-    "".into()
+	let mut ops: Vec<(&str, i32)> = vec![];
+	for line in input.lines() {
+		let (op, v) = line.split_delim(' ');
+		ops.push((op, v.parse::<i32>().unwrap()));
+	}
+
+	let len = ops.len();
+	let mut guess = 0;
+	while guess < len {
+		let (op, qt) = ops[guess];
+		if op != "jmp" {
+			guess += 1;
+			continue;
+		}
+		let mut visited = HashSet::new();
+		let mut idx: usize = 0;
+		let mut v = 0;
+		loop {
+			if idx == len {
+				return v.to_string();
+			}
+			if idx > len || visited.contains(&idx) {
+				break;
+			}
+			visited.insert(idx);
+			if idx == guess {
+				idx += 1;
+				continue;
+			}
+			match ops[idx].0 {
+				"acc" => {
+					v += ops[idx].1;
+					idx += 1;
+				}
+				"jmp" => {
+					idx = (idx as i32 + ops[idx].1) as usize;
+				}
+				"nop" => {
+					idx += 1;
+				}
+				_ => panic!("{}", ops[idx].0),
+			}
+		}
+		guess += 1;
+	}
+
+	panic!("Failed!");
 }
 
 #[allow(dead_code)]
@@ -88,12 +134,12 @@ mod tests {
     #[test]
     fn p2s() {
         let input = util::read_file("inputs/2020/day8-sample.txt");
-        assert_eq!("", part2(input));
+        assert_eq!("8", part2(input));
     }
 
     #[test]
     fn p2() {
         let input = util::read_file("inputs/2020/day8.txt");
-        assert_eq!("", part2(input));
+        assert_eq!("2096", part2(input));
     }
 }
