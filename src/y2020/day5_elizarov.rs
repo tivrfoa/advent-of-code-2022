43561bbc2609eda7d5072d30fe55d74cec17578e
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-11-07 19:28:36 -0300

    y2020 d5 add Elizarov solution

---
+++ src/y2020/day5_elizarov.rs
@@ -0,0 +1,120 @@
+use crate::util;
+
+use std::cell::Cell;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::fmt::{Debug, Display};
+use std::hash::Hash;
+use std::iter::zip;
+
+use util::*;
+
+fn part1(input: String) -> String {
+	let mut max = 0;
+
+	for line in input.lines() {
+		let mut l = 0;
+		let mut r = 127;
+		for c in line[0..7].chars() {
+			if c == 'F' {
+				r = (l + r) / 2;
+			} else {
+				l = (l + r) / 2 + 1;
+			}
+		}
+		assert!(l == r);
+		let row = l;
+
+		let mut l = 0;
+		let mut r = 7;
+		for c in line[7..].chars() {
+			if c == 'L' {
+				r = (l + r) / 2;
+			} else {
+				l = (l + r) / 2 + 1;
+			}
+		}
+		assert!(l == r);
+		let col = l;
+
+		let seat_id = row * 8 + col;
+		max = max.max(seat_id);
+	}
+
+	max.to_string()
+}
+
+fn part2(input: String) -> String {
+	let mut ids = HashSet::new();
+	for line in input.lines() {
+		let mut id = 0;
+		for c in line.chars() {
+			id <<= 1;
+			if c == 'B' || c == 'R' {
+				id |= 1;
+			}
+		}
+
+		ids.insert(id);
+	}
+	println!("{}", ids.iter().max().unwrap());
+
+	for i in 8..(1023 - 8) {
+		if !ids.contains(&i) && ids.contains(&(i + 1)) &&
+			ids.contains(&(i - 1)) {
+			return i.to_string();
+		}
+	}
+	panic!("failed");
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Eq, PartialEq)]
+struct State {
+    cost: u32,
+    position: (usize, usize),
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
+            .then_with(|| self.position.cmp(&other.position))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[ignore]
+    fn p1s() {
+        let input = util::read_file("inputs/2020/day5-sample.txt");
+        assert_eq!("", part1(input));
+    }
+
+    #[test]
+    fn p1() {
+        let input = util::read_file("inputs/2020/day5.txt");
+        assert_eq!("991", part1(input));
+    }
+
+    #[test]
+    #[ignore]
+    fn p2s() {
+        let input = util::read_file("inputs/2020/day5-sample.txt");
+        assert_eq!("", part2(input));
+    }
+
+    #[test]
+    fn p2() {
+        let input = util::read_file("inputs/2020/day5.txt");
+        assert_eq!("534", part2(input));
+    }
+}
