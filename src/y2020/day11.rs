acc66342d368a8e747c49e957626bae30e1a292c
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-11-09 08:12:16 -0300

    y2020 d11 solved p1

---
+++ src/y2020/day11.rs
@@ -0,0 +1,124 @@
+use crate::util;
+
+use std::cell::Cell;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::fmt::{Debug, Display};
+use std::hash::Hash;
+use std::iter::zip;
+
+use util::*;
+
+fn is_around_free(grid: &[Vec<char>], r: usize, c: usize) -> bool {
+	let rows = grid.len();
+	let cols = grid[0].len();
+	let dirs = get_dirs_with_diagonals(r, c, rows, cols);
+	for (cond, (r2, c2)) in dirs {
+		if cond && grid[r2][c2] == '#' {
+			return false;
+		}
+	}
+	true
+}
+
+fn is_around_occupied(grid: &[Vec<char>], r: usize, c: usize) -> bool {
+	let rows = grid.len();
+	let cols = grid[0].len();
+	let dirs = get_dirs_with_diagonals(r, c, rows, cols);
+	let mut qt = 0;
+	for (cond, (r2, c2)) in dirs {
+		if cond && grid[r2][c2] == '#' {
+			qt += 1;
+		}
+	}
+	qt >= 4
+}
+
+fn part1(input: String) -> String {
+	let mut grid = input.to_char_grid();
+	let rows = grid.len();
+	let cols = grid[0].len();
+
+	loop {
+		let mut changed = false;
+		let mut new_grid: Vec<Vec<char>> = grid.clone();
+
+		for r in 0..rows {
+			for c in 0..cols {
+				if grid[r][c] == 'L' {
+					if is_around_free(&grid, r, c) {
+						changed = true;
+						new_grid[r][c] = '#';
+					}
+				} else if grid[r][c] == '#' {
+					if is_around_occupied(&grid, r, c) {
+						changed = true;
+						new_grid[r][c] = 'L';
+					}
+				}
+			}
+		}
+
+		if !changed { break; }
+		grid = new_grid;
+	}
+
+	let mut ans = 0;
+	for row in grid {
+		ans += row.iter().filter(|&&c| c == '#').count();
+	}
+	ans.to_string()
+}
+
+fn part2(input: String) -> String {
+    "".into()
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Eq, PartialEq)]
+struct State {
+    cost: u32,
+    position: (usize, usize),
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
+            .then_with(|| self.position.cmp(&other.position))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn p1s() {
+        let input = util::read_file("inputs/2020/day11-sample.txt");
+        assert_eq!("37", part1(input));
+    }
+
+    #[test]
+    fn p1() {
+        let input = util::read_file("inputs/2020/day11.txt");
+        assert_eq!("2489", part1(input));
+    }
+
+    #[test]
+    fn p2s() {
+        let input = util::read_file("inputs/2020/day11-sample.txt");
+        assert_eq!("", part2(input));
+    }
+
+    #[test]
+    fn p2() {
+        let input = util::read_file("inputs/2020/day11.txt");
+        assert_eq!("", part2(input));
+    }
+}
