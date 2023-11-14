f30e9c91bb9941aa7e43859b496f5280083de563
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-11-14 05:48:39 -0300

    answer too low 213044139619

--- src/y2020/day16.rs
+++ src/y2020/day16.rs
@@ -65,7 +65,111 @@ fn part1(input: String) -> String {
 }
 
 fn part2(input: String) -> String {
-    "".into()
+	let mut ranges: Vec<(usize, usize, usize, usize)> = vec![];
+	let mut my_tickets: Vec<usize> = vec![];
+	let mut nearby_tickets: Vec<Vec<usize>> = vec![];
+	let mut read_my_tickets = false;
+	let mut read_nearby = false;
+
+	for line in input.lines() {
+		if line.starts_with("your") || line.starts_with("nearby") {
+			continue;
+		}
+		if line.is_empty() {
+			if read_my_tickets {
+				read_nearby = true;
+			} else {
+				read_my_tickets = true;
+			}
+			continue;
+		}
+		if read_nearby {
+			nearby_tickets.push(line.split_to_nums(','));
+		} else if read_my_tickets {
+			my_tickets = line.split_to_nums(',');
+		} else {
+			let tmp: Vec<&str> = line.split(':').collect();
+			let (l, r) = tmp[1].trim().split_once(" or ").unwrap();
+			let (l1, l2) = l.split_once('-').unwrap();
+			let (r1, r2) = r.split_once('-').unwrap();
+			ranges.push((
+				l1.parse().unwrap(),
+				l2.parse().unwrap(),
+				r1.parse().unwrap(),
+				r2.parse().unwrap()
+			));
+		}
+	}
+
+	let mut valid_tickets = vec![];
+	valid_tickets.push(&my_tickets);
+	'nearby:
+	for near in &nearby_tickets {
+		'n:
+		for n in near {
+			let n = *n;
+			for r in &ranges {
+				if (r.0 <= n && n <= r.1) || (r.2 <= n && n <= r.3) {
+					continue 'n;
+				}
+			}
+			continue 'nearby;
+		}
+		valid_tickets.push(near);
+	}
+	// dbg!(ranges, my_tickets, nearby_tickets);
+	let qt_tickets = valid_tickets.len();
+	let qt_fields = my_tickets.len();
+	assert!(qt_fields == ranges.len());
+	let mut in_range: Vec<Vec<usize>> = vec![vec![]; qt_fields];
+
+	for f in 0..qt_fields {
+		'c:
+		for c in 0..qt_fields {
+			for i in 0..valid_tickets.len() {
+				let n = valid_tickets[i][c];
+				if !((ranges[f].0 <= n && n <= ranges[f].1) ||
+					(ranges[f].2 <= n && n <= ranges[f].3)) {
+					continue 'c;
+				}
+			}
+			in_range[f].push(c);
+		}
+	}
+
+	dbg!(&in_range);
+	let mut order = vec![0; qt_fields];
+	if solve(&in_range, 0, 0, &mut order) {
+		let mut ans = 1;
+		for i in 0..6 {
+			ans *= my_tickets[order[i]];
+		}
+		ans.to_string()
+	} else {
+		panic!("Mission Failed!");
+	}
+}
+
+fn is_used(used: usize, pos: usize) -> bool {
+	(1 << pos) & used == 1
+}
+
+fn solve(in_range: &[Vec<usize>], field: usize, mut used: usize, order: &mut Vec<usize>) -> bool {
+	for c in &in_range[field] {
+		let c = *c;
+		if !is_used(used, c) {
+			used |= 1 << c;
+			if field + 1 == in_range.len() { // solved!
+				return true;
+			}
+			order[field] = c;
+			if solve(in_range, field + 1, used, order) {
+				return true;
+			}
+			used &= !(1 << c);
+		}
+	}
+	false
 }
 
 #[allow(dead_code)]
@@ -105,6 +209,7 @@ mod tests {
     }
 
     #[test]
+	#[ignore]
     fn p2s() {
         let input = util::read_file("inputs/2020/day16-sample.txt");
         assert_eq!("", part2(input));
