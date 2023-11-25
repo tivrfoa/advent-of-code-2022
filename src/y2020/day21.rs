a2bbe4254ac10dac00d6a86b80fda414143306ac
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-11-25 15:16:54 -0300

    cargo fmt

--- src/y2020/day21.rs
+++ src/y2020/day21.rs
@@ -10,66 +10,65 @@ use std::iter::zip;
 use util::*;
 
 fn filter<'a>(allergens: &mut HashMap<&'a str, Vec<&str>>, a: &'a str, ingredients: &Vec<&str>) {
-	let mut retain = vec![];
-	if let Some(foods) = allergens.get(a) {
-		for f in foods {
-			if ingredients.contains(f) {
-				retain.push(*f);
-			}
-		}
-	} else {
-		panic!("nothing to filter ...");
-	}
-	allergens.insert(a, retain);
+    let mut retain = vec![];
+    if let Some(foods) = allergens.get(a) {
+        for f in foods {
+            if ingredients.contains(f) {
+                retain.push(*f);
+            }
+        }
+    } else {
+        panic!("nothing to filter ...");
+    }
+    allergens.insert(a, retain);
 }
 
 fn part1(input: String) -> String {
-	let mut allergens: HashMap<&str, Vec<&str>> = HashMap::new();
-	let mut ingredients: Vec<&str> = vec![];
-	let mut used: HashSet<&str> = HashSet::new();
+    let mut allergens: HashMap<&str, Vec<&str>> = HashMap::new();
+    let mut ingredients: Vec<&str> = vec![];
+    let mut used: HashSet<&str> = HashSet::new();
 
-	for line in input.lines() {
-		let (ff, aa) = line.split_once(" (contains ").unwrap();
-		let mut ff: Vec<&str> = ff.split(" ").filter(|f| !used.contains(f)).collect();
-		let aa = aa.split_once(')').unwrap().0;
-		let aa: Vec<&str> = aa.split(", ").collect();
+    for line in input.lines() {
+        let (ff, aa) = line.split_once(" (contains ").unwrap();
+        let mut ff: Vec<&str> = ff.split(" ").filter(|f| !used.contains(f)).collect();
+        let aa = aa.split_once(')').unwrap().0;
+        let aa: Vec<&str> = aa.split(", ").collect();
 
-		for a in &aa {
-			if allergens.contains_key(a) {
-				filter(&mut allergens, a, &ff);
-				if allergens[a].len() == 1 {
-					let mut foods_to_remove: Vec<(&str, &str)> = vec![(a, allergens[a][0])];
-					while let Some((key, used_food)) = foods_to_remove.pop() {
-						used.insert(used_food);
-						// remove this food from other lists
-						for (k, v) in allergens.iter_mut() {
-							if k == &key { continue; }
-							if let Some(pos) = v.iter().position(|f| f == &used_food) {
-								v.remove(pos);
-								if v.len() == 1 {
-									foods_to_remove.push((k, v[0]));
-								}
-							}
-						}
-					}
-				}
-			} else {
-				allergens.insert(a, ff.clone());
-			}
-		}
+        for a in &aa {
+            if allergens.contains_key(a) {
+                filter(&mut allergens, a, &ff);
+                if allergens[a].len() == 1 {
+                    let mut foods_to_remove: Vec<(&str, &str)> = vec![(a, allergens[a][0])];
+                    while let Some((key, used_food)) = foods_to_remove.pop() {
+                        used.insert(used_food);
+                        // remove this food from other lists
+                        for (k, v) in allergens.iter_mut() {
+                            if k == &key {
+                                continue;
+                            }
+                            if let Some(pos) = v.iter().position(|f| f == &used_food) {
+                                v.remove(pos);
+                                if v.len() == 1 {
+                                    foods_to_remove.push((k, v[0]));
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                allergens.insert(a, ff.clone());
+            }
+        }
 
-		ingredients.append(&mut ff);
-		dbg!(&allergens, &used);
-	}
+        ingredients.append(&mut ff);
+        dbg!(&allergens, &used);
+    }
 
-	let mut qt = 0;
-	for i in ingredients {
-		if !used.contains(i) {
-			qt += 1;
-		}
-	}
-
-	qt.to_string()
+    ingredients
+        .into_iter()
+        .filter(|i| !used.contains(i))
+        .count()
+        .to_string()
 }
 
 fn part2(input: String) -> String {
@@ -85,7 +84,9 @@ struct State {
 
 impl Ord for State {
     fn cmp(&self, other: &Self) -> Ordering {
-        other.cost.cmp(&self.cost)
+        other
+            .cost
+            .cmp(&self.cost)
             .then_with(|| self.position.cmp(&other.position))
     }
 }
