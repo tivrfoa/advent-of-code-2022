13a7017309efe41b53265bac000fab945342ee0f
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-11-25 19:32:57 -0300

    refactor

--- src/y2020/day21.rs
+++ src/y2020/day21.rs
@@ -12,7 +12,6 @@ use util::*;
 fn part1(input: String) -> String {
     let mut allergens: HashMap<&str, Vec<&str>> = HashMap::new();
     let mut ingredients: Vec<&str> = vec![];
-    let mut used: HashSet<&str> = HashSet::new();
 
     for line in input.lines() {
         let (ff, aa) = line.split_once(" (contains ").unwrap();
@@ -29,7 +28,6 @@ fn part1(input: String) -> String {
                 if v.len() == 1 {
                     let mut foods_to_remove: Vec<(&str, &str)> = vec![(a, v[0])];
                     while let Some((key, used_food)) = foods_to_remove.pop() {
-                        used.insert(used_food);
                         // remove this food from other lists
                         for (k, v) in allergens.iter_mut() {
                             if k == &key {
@@ -52,33 +50,29 @@ fn part1(input: String) -> String {
         ingredients.extend(ff);
     }
 
+	let unsafe_ingredients: Vec<&str> = allergens.values().map(|v| v[0]).collect();
     ingredients
         .into_iter()
-        .filter(|i| !used.contains(i))
+        .filter(|i| !unsafe_ingredients.contains(i))
         .count()
         .to_string()
 }
 
 fn part2(input: String) -> String {
     let mut allergens: BTreeMap<&str, Vec<&str>> = BTreeMap::new();
-    let mut used: HashSet<&str> = HashSet::new();
-
     for line in input.lines() {
         let (ff, aa) = line.split_once(" (contains ").unwrap();
         let ff: Vec<&str> = ff.split(" ").collect();
         let aa = aa.split_once(')').unwrap().0;
-
         for a in aa.split(", ") {
 			if let Some(v) = allergens.get_mut(a) {
 				*v = v.into_iter()
 					.filter(|f| ff.contains(f))
 					.map(|f| *f)
 					.collect();
-
                 if v.len() == 1 {
                     let mut foods_to_remove: Vec<(&str, &str)> = vec![(a, v[0])];
                     while let Some((key, used_food)) = foods_to_remove.pop() {
-                        used.insert(used_food);
                         // remove this food from other lists
                         for (k, v) in allergens.iter_mut() {
                             if k == &key {
@@ -99,15 +93,7 @@ fn part2(input: String) -> String {
         }
     }
 
-	let mut ans = String::new();
-	for (k, ii) in allergens {
-		if ii.len() != 1 {
-			panic!("Failed for {k}: {:?}", ii);
-		}
-		ans.push_str(ii[0]);
-		ans.push(',');
-	}
-	(&ans[..ans.len() - 1]).into()
+	allergens.values().map(|v| v[0]).collect::<Vec<&str>>().join(",")
 }
 
 #[allow(dead_code)]
