beedc11e6ebd3279c28869eba7b9db0c21bc6f4f
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-12-01 09:05:48 -0300

    52870 too high

--- src/y2023/day1.rs
+++ src/y2023/day1.rs
@@ -39,60 +39,53 @@ pub fn part1(input: String) -> String {
 }
 
 pub fn part2(input: String) -> String {
+	const DIGITS: [(&str, u32); 18] = [
+		("1", 1),
+		("2", 2),
+		("3", 3),
+		("4", 4),
+		("5", 5),
+		("6", 6),
+		("7", 7),
+		("8", 8),
+		("9", 9),
+		("one", 1),
+		("two", 2),
+		("three", 3),
+		("four", 4),
+		("five", 5),
+		("six", 6),
+		("seven", 7),
+		("eight", 8),
+		("nine", 9),
+	];
 	let mut sum = 0;
-	let mut digits: [(usize, u32); 2] = [(0, 0); 2];
 	for line in input.lines() {
 		let mut idx = 0;
-		digits[1] = (0, 10);
-		for (i, c) in line.chars().enumerate() {
-			match c.to_digit(10) {
-				Some(n) => {
-					digits[idx] = (i, n);
-					if idx == 0 {
-						idx += 1;
-					}
-				}
-				None => {
-				}
-			}
-		}
+		let mut digits: [(usize, u32); 2] = [(0, 10); 2];
 
-		if idx == 0 {
-			digits[0] = (line.len(), 0);
-			digits[1] = (0, 0);
-		}
-
-		const DIGITS: [&str; 9] = [
-			"one", "two", "three", "four", "five", "six", "seven",
-			"eight", "nine"
-		];
-
-		let mut first = digits[0].0;
-		for (i, d) in DIGITS.iter().enumerate() {
-			if let Some(p) = line.find(d) {
+		let mut first = line.len();
+		for (sval, val) in DIGITS {
+			if let Some(p) = line.find(sval) {
 				if p < first {
 					first = p;
-					digits[0] = (p, i as u32 + 1);
+					digits[0] = (p, val);
 				}
 			}
 		}
+		digits[1] = digits[0];
 
-		let mut second = digits[1].0;
-		for (i, d) in DIGITS.iter().enumerate() {
-			if let Some(p) = line[digits[1].0+1..].find(d) {
-				let p = p + digits[1].0+1;
-				println!("Found {p}, second = {}, i = {i}", second);
+		let mut second = first + 1;
+		for (sval, val) in DIGITS {
+			if let Some(p) = line[second..].find(sval) {
+				let p = p + second;
 				if p > second {
 					second = p;
-					digits[1] = (p, i as u32 + 1);
+					digits[1] = (p, val);
 				}
 			}
 		}
 		dbg!(&digits);
-
-		if digits[1].1 == 10 {
-			digits[1] = digits[0];
-		}
 
 		sum += digits[0].1 * 10 + digits[1].1;
 	}
