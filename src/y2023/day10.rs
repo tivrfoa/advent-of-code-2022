5a329eb7a7bc71a2384c12ee70444319137e8e63
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-12-10 09:45:55 -0300

    p2 too high: 698

--- src/y2023/day10.rs
+++ src/y2023/day10.rs
@@ -198,7 +198,228 @@ pub fn part1(input: &str) -> String {
 }
 
 pub fn part2(input: &str) -> String {
-    "".into()
+    // lp: mark positions that belong to the main loop
+    // for every other position, try to make it go outside without
+    // passing through loop positions (lp)
+
+    let mut grid = input.to_char_grid();
+    let rows = grid.len();
+    let cols = grid[0].len();
+    let start = {
+        let mut S = (0, 0);
+        'r:
+        for r in 0..rows {
+            for c in 0..cols {
+                if grid[r][c] == 'S' {
+                    S = (r, c);
+                    break 'r;
+                }
+            }
+        }
+        S
+    };
+
+    // finding the loop: try each of the possible pipes and check if it's a loop
+    for pipe in PIPES {
+        grid[start.0][start.1] = pipe;
+        if let Some(lp) = solve_p2(&grid, start.0, start.1) {
+            return count_enclosed(&grid, lp).to_string();
+        }
+    }
+
+    "failed".into()
+}
+
+type Pos = (usize, usize);
+
+fn can_go_outside(grid: &[Vec<char>], lp: &[Pos], r: usize, c: usize,
+        visited: &mut HashSet<Pos>) -> bool {
+    if !visited.insert((r, c)) || lp.contains(&(r, c)) { return false; }
+    let rows = grid.len();
+    let cols = grid[0].len();
+    if r == 0 || r + 1 == rows || c == 0 || c + 1 == cols {
+        return true;
+    }
+
+    for dy in -1..=1 {
+        for dx in -1..=1 {
+            if can_go_outside(grid, lp, (r as i32 + dy) as usize, ((c as i32) + dx) as usize, visited) {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+fn count_enclosed(grid: &[Vec<char>], lp: Vec<Pos>) -> usize {
+    let rows = grid.len();
+    let cols = grid[0].len();
+    let mut qt = 0;
+
+    for r in 0..rows {
+        for c in 0..cols {
+            if lp.contains(&(r, c)) { continue; }
+            let mut visited: HashSet<Pos> = HashSet::new();
+            if !can_go_outside(grid, &lp, r, c, &mut visited) {
+                qt += 1;
+            }
+        }
+    }
+
+    qt
+}
+
+fn solve_p2(grid: &[Vec<char>], start_row: usize, start_col: usize) -> Option<Vec<(usize, usize)>> {
+    let rows = grid.len();
+    let cols = grid[0].len();
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    let mut r = start_row;
+    let mut c = start_col;
+    let mut from = (r, c);
+    let mut lp = vec![];
+
+    loop {
+        if !visited.insert((r, c)) {
+            return None;
+        }
+        lp.push((r, c));
+
+        match grid[r][c] {
+            '|' => {
+                // north
+                if r > 0 && (r - 1, c) != from {
+                    from = (r, c);
+                    r = r - 1;
+                    let d = grid[r][c];
+                    if d != '|' && d != '7' && d != 'F' {
+                        return None;
+                    }
+                    // south
+                } else if r + 1 < rows && (r + 1, c) != from {
+                    from = (r, c);
+                    r = r + 1;
+                    let d = grid[r][c];
+                    if d != '|' && d != 'L' && d != 'J' {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            }
+            '-' => {
+                // west
+                if c > 0 && (r, c - 1) != from {
+                    from = (r, c);
+                    c = c - 1;
+                    let d = grid[r][c];
+                    if d != '-' && d != 'L' && d != 'F' {
+                        return None;
+                    }
+                    // east
+                } else if c + 1 < cols && (r, c + 1) != from {
+                    from = (r, c);
+                    c = c + 1;
+                    let d = grid[r][c];
+                    if d != '-' && d != 'J' && d != '7' {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            }
+            'L' => {
+                // east
+                if c + 1 < cols && (r, c + 1) != from {
+                    from = (r, c);
+                    c += 1;
+                    let d = grid[r][c];
+                    if d != '-' && d != 'J' && d != '7' {
+                        return None;
+                    }
+                    // north
+                } else if r > 0 && (r - 1, c) != from {
+                    from = (r, c);
+                    r -= 1;
+                    let d = grid[r][c];
+                    if d != '|' && d != '7' && d != 'F' {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            }
+            'J' => {
+                // north
+                if r > 0 && (r - 1, c) != from {
+                    from = (r, c);
+                    r -= 1;
+                    let d = grid[r][c];
+                    if d != '|' && d != '7' && d != 'F' {
+                        return None;
+                    }
+                    // west
+                } else if c > 0 && (r, c - 1) != from {
+                    from = (r, c);
+                    c -= 1;
+                    let d = grid[r][c];
+                    if d != '-' && d != 'L' && d != 'F' {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            }
+            '7' => {
+                // south
+                if r + 1 < rows && (r + 1, c) != from {
+                    from = (r, c);
+                    r += 1;
+                    let d = grid[r][c];
+                    if d != '|' && d != 'L' && d != 'J' {
+                        return None;
+                    }
+                    // west
+                } else if c > 0 && (r, c - 1) != from {
+                    from = (r, c);
+                    c -= 1;
+                    let d = grid[r][c];
+                    if d != '-' && d != 'L' && d != 'F' {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            }
+            'F' => {
+                // south
+                if r + 1 < rows && (r + 1, c) != from {
+                    from = (r, c);
+                    r += 1;
+                    let d = grid[r][c];
+                    if d != '|' && d != 'L' && d != 'J' {
+                        return None;
+                    }
+                    // east
+                } else if c + 1 < cols && (r, c + 1) != from {
+                    from = (r, c);
+                    c += 1;
+                    let d = grid[r][c];
+                    if d != '-' && d != 'J' && d != '7' {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            }
+            _ => panic!("{}", grid[r][c]),
+        }
+
+        if r == start_row && c == start_col {
+            // found the loop!
+            return Some(lp);
+        }
+    }
 }
 
 #[allow(dead_code)]
@@ -247,8 +468,8 @@ mod tests {
 
     #[test]
     fn p2s() {
-        let input = include_str!("../../inputs/2023/day10-sample.txt");
-        assert_eq!("", part2(input));
+        let input = include_str!("../../inputs/2023/day10-sample-p2-01.txt");
+        assert_eq!("10", part2(input));
     }
 
     #[test]
