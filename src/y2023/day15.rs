bc1f1c179328a96e309ac60e5a60d2c502602a59
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-12-15 09:14:10 -0300

    2023 15 solved p2

--- src/y2023/day15.rs
+++ src/y2023/day15.rs
@@ -9,47 +9,70 @@ use std::iter::zip;
 
 use util::*;
 
+fn hash(s: &str) -> usize {
+    let mut curr = 0;
+    for c in s.chars() {
+        curr += c as usize;
+        curr *= 17;
+        curr %= 256;
+    }
+    curr
+}
+
 pub fn part1(input: &str) -> String {
     let mut sum = 0;
     let line = input.lines().next().unwrap();
 
     for s in line.split(',') {
-        let mut curr: u64 = 0;
-        for c in s.chars() {
-            curr += c as u64;
-            curr *= 17;
-            curr %= 256;
-        }
-        sum += curr;
+        sum += hash(s);
     }
 
     sum.to_string()
 }
 
+fn order_boxes(boxes: Vec<(HashMap<&str, (&str, usize)>, usize)>) -> Vec<Vec<usize>> {
+    let mut ordered_boxes = vec![vec![]; 256];
+
+    for (i, b) in boxes.into_iter().enumerate() {
+        let mut v: Vec<(&str, usize)> = b.0.into_values().collect();
+        v.sort_by(|a, b| a.1.cmp(&b.1));
+        ordered_boxes[i] = v.into_iter().map(|v| v.0.parse::<usize>().unwrap()).collect();
+    }
+
+    ordered_boxes
+}
+
 pub fn part2(input: &str) -> String {
-    "".into()
-}
+    let line = input.lines().next().unwrap();
+    let mut boxes: Vec<(HashMap<&str, (&str, usize)>, usize)> = vec![(HashMap::new(), 0); 256];
 
-#[allow(dead_code)]
-#[derive(Clone, Eq, PartialEq)]
-struct State {
-    cost: u32,
-    position: (usize, usize),
-}
-
-impl Ord for State {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other
-            .cost
-            .cmp(&self.cost)
-            .then_with(|| self.position.cmp(&other.position))
+    for s in line.split(',') {
+        if s.contains('=') {
+            let (l, r) = s.split_once('=').unwrap();
+            let b = hash(l);
+            if let Some(e) = boxes[b].0.get_mut(&l) {
+                e.0 = r;
+            } else {
+                let pos = boxes[b].1;
+                boxes[b].1 += 1;
+                boxes[b].0.insert(l, (r, pos));
+            }
+        } else {
+            let (l, _) = s.split_once('-').unwrap();
+            let b = hash(l);
+            boxes[b].0.remove(l);
+        }
     }
-}
 
-impl PartialOrd for State {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
+    let boxes: Vec<Vec<usize>> = order_boxes(boxes);
+    let mut sum = 0;
+    for (i, b) in boxes.into_iter().enumerate() {
+        for (j, l) in b.into_iter().enumerate() {
+            sum += (i + 1) * (j + 1) * l;
+        }
     }
+
+    sum.to_string()
 }
 
 #[cfg(test)]
@@ -71,12 +94,12 @@ mod tests {
     #[test]
     fn p2s() {
         let input = include_str!("../../inputs/2023/day15-sample.txt");
-        assert_eq!("", part2(input));
+        assert_eq!("145", part2(input));
     }
 
     #[test]
     fn p2() {
         let input = include_str!("../../inputs/2023/day15.txt");
-        assert_eq!("", part2(input));
+        assert_eq!("230197", part2(input));
     }
 }
